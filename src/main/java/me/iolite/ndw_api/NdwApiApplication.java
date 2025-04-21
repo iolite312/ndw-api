a46826fdd14b1dd1eebd96aaa6257af9618db029
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-04-21 19:41:16 +0200

    Initial commit

---
+++ src/main/java/me/iolite/ndw_api/NdwApiApplication.java
@@ -0,0 +1,13 @@
+package me.iolite.ndw_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdwApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NdwApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/iolite/ndw_api/NdwApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.iolite.ndw_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdwApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
